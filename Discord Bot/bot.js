3346838fa07f0edde251cb4ecd9bd287eefaf914
wezu123 <31524261+wezu123@users.noreply.github.com>
2019-02-16 22:36:26 +0100

    +Command support added
    +Bot will now respond to messages that start with his prefix, currently with few commands and a help command.
    +Started using JSON for safety reasons, and future functions.
    +Voice status updates limited to selected channel category, added for future updates

--- Discord Bot/bot.js
+++ Discord Bot/bot.js
@@ -1,48 +1,101 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const config = require("./config.json");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    var generalChannel = client.channels.get("343067505864212480");
+    console.log(`Connected as ${client.user.tag}`)
     console.log(client.readyAt)
-    var usedChannel = client.channels.get("343067505864212480");
+    console.log(`Loaded with prefix: ${config.prefix}`)
+    start();
+});
 
-  if(usedChannel != undefined){
+client.on('message', (message) => {
+  var generalChannel = client.channels.get("343067505864212480");
+
+  if(message.author.bot == false && message.content.indexOf(config.prefix) == 0){
+    var command = message.content.split(' ');
+    var action = command[0].slice(1);
+    console.log(command)
+
+    switch (action) {
+      case 'play':
+        message.channel.send(`Playing ` + command[1]);
+      break;
+      case 'joke':
+        message.channel.send(config.joke);
+      break;
+      case 'mute':
+        if(command[1] != undefined){
+          message.channel.send(`User ` + command[1] + ' has been muted');
+        }else{
+          message.channel.send('No user chosen, no action has been made');
+      }
+      break;
+      case 'help':
+        message.channel.send(`**Current commands**: play, joke, mute, help \n**Current prefix**: ${config.prefix}`);
+      break;
+      case 'prefix':
+        if(command[1] != undefined){
+          config.prefix = command[1];
+          message.channel.send(`**Prefix** set to ${config.prefix} for this session. It will change to default once the bot is restarted.`);
+        }else{
+          message.channel.send('Empty paramater, parse the prefix after whitespace');
+        }
+      break;
+      default:
+        message.channel.send(`Incorrect command, please use ${config.prefix}help for more information`);
+      }
+    }
+});
+
+function start(){
+  var generalChannel = client.channels.get("343067505864212480");
+  var usedChannel = client.channels.get("343067505864212480");
+
+  if(usedChannel !== undefined){
+    console.log("Bot channel defined")
+
     client.on('voiceStateUpdate', (oldMember, newMember) => {
       let newUserChannel = newMember.voiceChannel;
       let oldUserChannel = oldMember.voiceChannel;
-      let generalChannel = client.channels.get("343067505864212480"); // Replace with known channel ID
 
       if(oldUserChannel === undefined && newUserChannel !== undefined) {
-        generalChannel.send({embed: {
-            color: 0x20cc2c,
-            author: {
-              name: newMember.user.tag,
-              icon_url: newMember.user.avatarURL
-            },
-            description: ("<@" + newMember.id + "> has entered the channel " + "**" + newUserChannel + "**"),
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: "ID: " + newMember.id
+        if(newUserChannel.parent.id == '423153937084973057'){
+          generalChannel.send({embed: {
+              color: 0x42f456,
+              author: {
+                name: newMember.user.tag,
+                icon_url: newMember.user.avatarURL
+              },
+              description: (`<@${newMember.id}> has entered the channel ** ${newUserChannel} **`),
+              timestamp: new Date(),
+              footer: {
+                //icon_url: client.user.avatarURL,
+                text: "ID: " + newMember.id
+              }
             }
-          }
-        });
+          });
+        }else{console.log("Wrong category 0")}
       } else if(newUserChannel !== undefined && oldUserChannel !== undefined){
-        generalChannel.send({embed: {
-            color: 0x267fe5,
-            author: {
-              name: newMember.user.tag,
-              icon_url: newMember.user.avatarURL
-            },
-            description: ("<@" + newMember.id + "> has switched the channel to " + "**" + newUserChannel + "**"),
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: "ID: " + newMember.id
+        if(newUserChannel.parent.id == '423153937084973057'){
+          generalChannel.send({embed: {
+              color: 0x267fe5,
+              author: {
+                name: newMember.user.tag,
+                icon_url: newMember.user.avatarURL
+              },
+              description: (`<@${newMember.id}> has switched the channel to **${newUserChannel}**`),
+              timestamp: new Date(),
+              footer: {
+                //icon_url: client.user.avatarURL,
+                text: "ID: " + newMember.id
+              }
             }
-          }
-        });
+          });
+        }else{console.log("Wrong category 1")}
       } else if(newUserChannel === undefined){
+        if(oldUserChannel.parent.id == '423153937084973057'){
           generalChannel.send({embed: {
             color: 0xe52727,
             author: {
@@ -52,14 +105,16 @@ client.on('ready', () => {
             description: ("<@" + oldMember.id + "> has left the channel " + "**" + oldUserChannel + "**"),
             timestamp: new Date(),
             footer: {
-              icon_url: client.user.avatarURL,
+              //icon_url: client.user.avatarURL,
               text: "ID: " + oldMember.id
             }
           }
-        });
+          });
+        }else{console.log("Wrong category 2")}
       }
     });
   }
-})
+}
 
-client.login("NTQxNDI5MDQ1NTA2NzM2MTI5.Dzzj3Q.-ll0Ed65EmkJoz-U6t79KOgwTw8")
+client.login(config.token)
+//I just want to know why do you look for unsecured bots on GitHub all the time.
