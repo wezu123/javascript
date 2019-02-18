ffd41e07c1cce9d1125f90e868399266739b8170
wezu123 <31524261+wezu123@users.noreply.github.com>
2019-02-18 21:04:51 +0100

    Writing user settings to file and new commands
    +Used fs for Node.js to manage user settings
    +Prefix changing command is now fully working and will save user changes to the file
    +Added basic setting commands for voice channels
    +Bot won't save the same channel multiple times
    +Cleared out the code a bit

--- Discord Bot/bot.js
+++ Discord Bot/bot.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
+const fs = require('fs');
+const configRead = fs.readFileSync('config.json');
+const configFile = 'config.json';
+const config = JSON.parse(configRead);
 const client = new Discord.Client();
+var voice = new Array;
 
 client.on('ready', () => {
     var generalChannel = client.channels.get("343067505864212480");
@@ -21,28 +25,47 @@ client.on('message', (message) => {
     console.log(command)
 
     switch (action) {
-      case 'play':
-        message.channel.send(`Playing ` + command[1]);
-      break;
-      case 'joke':
+    /*case 'joke':
         message.channel.send(config.joke);
-      break;
-      case 'mute':
-        if(command[1] != undefined){
-          message.channel.send(`User ` + command[1] + ' has been muted');
-        }else{
-          message.channel.send('No user chosen, no action has been made');
-      }
-      break;
+      break;*/
       case 'help':
         message.channel.send(`**Current commands**: play, joke, mute, help \n**Current prefix**: ${config.prefix}`);
       break;
       case 'prefix':
         if(command[1] != undefined){
           config.prefix = command[1];
-          message.channel.send(`**Prefix** set to ${config.prefix} for this session. It will change to default once the bot is restarted.`);
+          message.channel.send(`**Prefix** set to ${config.prefix}.`);
+          fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
         }else{
           message.channel.send('Empty paramater, parse the prefix after whitespace');
+        }
+      break;
+      case 'voice':
+        if(command[1] == "set"){
+          voice = [];
+          voice[0] = message.member.voiceChannel;
+          console.log(voice)
+        }else if(command[1] == "reset"){
+          voice = [];
+          console.log(voice)
+        }else if(command[1] == "add"){
+          if(voice.includes(message.member.voiceChannel) == false){
+            voice.push(message.member.voiceChannel);
+            console.log(voice[voice.length - 1].name)
+          }else{
+            console.log("Channel already included")
+          }
+        }else if(command[1] == "parent"){
+          console.log(message.member.voiceChannel.parent.name)
+        }else if(command[1] == "log"){
+          for(let i=0;i<voice.length;i++){
+            console.log(voice[i].name)
+          }
+        }
+      break;
+      case 'note':
+        if(command[0] == "set"){
+          noteChannel = message.channel;
         }
       break;
       default:
@@ -55,9 +78,7 @@ client.on('message', (message) => {
 
 function start(){
   var generalChannel = client.channels.get("343067505864212480");
-  var usedChannel = client.channels.get("343067505864212480");
-
-  if(usedChannel !== undefined){
+  if(generalChannel !== undefined){
     console.log("Bot channel defined")
 
     client.on('voiceStateUpdate', (oldMember, newMember) => {
@@ -117,7 +138,9 @@ function start(){
         }else{console.log("Wrong category 2")}
       }
     });
-  }
+  }else{console.log("Undefined channel")}
 }
+
+//////////////////////////////////////////////////////////////////////////////
 
 client.login(config.token)
