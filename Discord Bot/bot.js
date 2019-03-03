877cdfa37a3bfa0c7646c04ba5b139436f38092b
wezu123 <31524261+wezu123@users.noreply.github.com>
2019-03-03 19:19:10 +0100

    Global log/clear, permissions and switch/leave settings.
    +Added global versions of log and clear commands, clearing/logging parents and voice channels with one command.
    +Commands will now not work unless you have Administrator privellege
    +Added notification message after adding new voice channel
    +Left some comments in the code for better readibility
    +New commands for disabling switch or leave notifications.
    +Full help message hooked up from JSON
    +Log messages now are displayed in a code block for better readibility

--- Discord Bot/bot.js
+++ Discord Bot/bot.js
@@ -4,22 +4,22 @@ const configRead = fs.readFileSync('config.json');
 const configFile = 'config.json';
 const config = JSON.parse(configRead);
 const client = new Discord.Client();
-global.note;
-global.log = new Array;
-global.parentLog = new Array;
+global.note; global.parentMsg = ''; global.voiceMsg = '';
+global.log = new Array; global.parentLog = new Array;
 
 client.on('ready', () => {
     global.note = client.channels.find(channel => channel.id == config.note)
     console.log(`Connected as ${client.user.tag}`)
     console.log(client.readyAt)
     console.log(`Loaded with prefix: ${config.prefix}`)
-    console.log("Note channel is " + global.note.name)
+    console.log(`Note channel is ${global.note.name}`)
 });
 
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////// MESSAGE EVENT  //////////////////////////////////
 
 client.on('message', (message) => {
   if(message.author.bot == false && message.content.startsWith(config.prefix) == true){
+    if(message.member.highestRole.hasPermission('ADMINISTRATOR') == true){
     var command = message.content.split(' ');
     var action = command[0].slice(config.prefix.length);
     console.log(command)
@@ -30,7 +30,8 @@ client.on('message', (message) => {
       break;
 
       case 'help':
-        message.channel.send(`**Current commands**: there are a lot of em lmao i aint gonna list em all \n**Current prefix**: ${config.prefix}`);
+        //message.channel.send(`**Current commands**: WIP \n**Current prefix**: ${config.prefix}`);
+        message.channel.send(`**Current commands**: ${config.help}`);
       break;
 
       case 'prefix':
@@ -41,6 +42,8 @@ client.on('message', (message) => {
           message.channel.send('Empty paramater, parse the prefix after whitespace');
         }
       break;
+
+/////////////////////////////// VOICE CHANNELS ///////////////////////////////
 
       case 'voice':
         if(command[1] == "set"){
@@ -58,25 +61,27 @@ client.on('message', (message) => {
         else if(command[1] == "add"){
           if(config.voice.includes(message.member.voiceChannel.id) == false){
             config.voice.push(message.member.voiceChannel.id);
-            console.log(config.voice[config.voice.length - 1].name)
+            message.channel.send("Voice channel added")
           }else{
             console.log("Channel already included")
           }
         }
-
         else if(command[1] == "log"){
           global.log = [];
           for(let i=0;config.voice.length>i;i++){
             global.log[i] = client.channels.find(channel => channel.id == config.voice[i]);
           }
-          if(global.log != undefined){
-            message.channel.send("List of voice channels:")
+          if(global.log[0] != undefined){
+            global.voiceMsg = "List of voice channels: \n"
             for(let i=0;i<global.log.length;i++){
-              message.channel.send("**#" + global.log[i].name + "**")
+              global.voiceMsg += ("#" + global.log[i].name +'\n')
             }
+            message.channel.send("```" + global.voiceMsg + "```")
           }else{message.channel.send("No voice channels defined")}
         }else{message.channel.send("Incorrect use of the command")}
       break;
+
+////////////////////////////// NOTE CHANNEL /////////////////////////////////
 
       case 'note':
         if(command[1] == "set"){
@@ -84,8 +89,27 @@ client.on('message', (message) => {
           console.log(message.channel.name)
           global.note = client.channels.find(channel => channel.id == config.note)
           global.note.send(`Notification channel for voice updates set to **#${global.note.name}**.`)
-        }else{message.channel.send("Incorrect use of the command")}
+        }
+        else if(command[1] == 'switch'){
+          if(command[2] == 0 || command[2] == 1){
+            config.switch = command[2];
+            if(config.switch == 0){
+              message.channel.send("Switch notifications turned off.")
+            }else{message.channel.send("Switch notifications turned on.")}
+          }else{message.channel.send("Wrong paramater, expected 0 or 1.")}
+        }
+        else if(command[1] == 'leave'){
+          if(command[2] == 0 || command[2] == 1){
+            config.leave = command[2];
+            if(config.leave == 0){
+              message.channel.send("Leave notifications turned off.")
+            }else{message.channel.send("Leaving notifications turned on.")}
+          }else{message.channel.send("Wrong paramater, expected 0 or 1.")}
+        }
+        else{message.channel.send("Incorrect use of the command")}
       break;
+
+//////////////////////////////// PARENT //////////////////////////////////////
 
       case 'parent':
         if(command[1] == 'set'){
@@ -110,24 +134,60 @@ client.on('message', (message) => {
             global.parentLog[i] = client.channels.find(channel => channel.id == config.parent[i]);
           }
           if(global.parentLog[0] != undefined){
-            message.channel.send("List of categories:")
+            global.parentMsg = "List of categories:\n"
             for(let i=0;i<global.parentLog.length;i++){
-              message.channel.send("**" + global.parentLog[i].name + "**")
+              global.parentMsg += (global.parentLog[i].name + "\n")
             }
+            message.channel.send("```" + global.parentMsg + "```");
           }else{message.channel.send("No categories defined")}
         }else{message.channel.send("Incorrect use of the command")}
 
+////////////////////////////////// UTILITY ////////////////////////////////////
+
       break;
+      case 'clear':
+        config.parent = []; config.voice = [];
+        message.channel.send('All categories and voice channels cleared.')
+      break;
+
+      case 'log':
+        global.log = [];  global.parentLog = []; global.parentMsg = ''; global.voiceMsg = '';
+
+        for(let i=0;config.voice.length>i;i++){
+          global.log[i] = client.channels.find(channel => channel.id == config.voice[i]);
+        }
+        if(global.log[0] != undefined){
+          global.voiceMsg = "List of voice channels: \n"
+          for(let i=0;i<global.log.length;i++){
+            global.voiceMsg += ("#" + global.log[i].name +'\n')
+          }
+        }
+
+        for(let i=0;config.parent.length>i;i++){
+          global.parentLog[i] = client.channels.find(channel => channel.id == config.parent[i]);
+        }
+        if(global.parentLog[0] != undefined){
+          global.parentMsg = "List of categories: \n"
+          for(let i=0;i<global.parentLog.length;i++){
+            global.parentMsg += (global.parentLog[i].name + "\n")
+          }
+        }
+        if(global.parentMsg != '' || global.voiceMsg != ''){
+          message.channel.send('```' + global.voiceMsg + '\n' + global.parentMsg + '```')
+        }else{message.channel.send('No categories or voice channels defined.')}
+      break;
+
       default:
         message.channel.send(`Incorrect command, please use ${config.prefix}help for more information`);
       }
       //if(JSON.stringify(config, null, 2) != JSON.parse(configRead)){
         fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
       //}
+    }else{message.channel.send("Insufficient permissions, please contact your server administrator.")}
     }
 });
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////// STATE UPDATE ///////////////////////////////////
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   if(global.note !== undefined){
@@ -150,7 +210,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
           }
         });
       }//else{console.log("Wrong join")}
-    }else if(newUserChannel !== undefined && oldUserChannel !== undefined){
+    }else if(newUserChannel !== undefined && oldUserChannel !== undefined && config.switch == 1){
       if(config.parent.includes(newUserChannel.parent.id) || config.voice.includes(newUserChannel.id)){
         global.note.send({embed: {
             color: 0x267fe5,
@@ -166,7 +226,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
           }
         });
       }//else{console.log("Wrong switch")}
-    } else if(newUserChannel === undefined){
+    } else if(newUserChannel === undefined && config.leave == 1){
       if(config.parent.includes(oldUserChannel.parent.id) || config.voice.includes(oldUserChannel.id)){
         global.note.send({embed: {
           color: 0xe52727,
@@ -185,9 +245,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     }
   }else{console.log("Undefined channel")}
 });
-//////////////////////////////////////////////////////////////////////////////
 
-
-
+///////////////////////////// /// LOGIN ///////////////////////////////////////
 
 client.login(config.token)
